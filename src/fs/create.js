a5c5fdbc05c058b153b5658a0020bc8f60f2d0b3
Tuychiev Otabek <otabekotabek996996@gmail.com>
2025-04-26 09:51:52 +0500

    file system (src/fs) - create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
-const create = async () => {
-    // Write your code here 
+import { writeFile, access } from "fs/promises";
+import { constants } from "fs";
+
+const fileName = "src/fs/files/fresh.txt";
+const content = "I am fresh and young";
+const errorMessage = "FS operation failed";
+
+const create = async (fileName, fileContent, errorMessage) => {
+  try {
+    await access(fileName, constants.F_OK);
+    console.log(errorMessage);
+  } catch {
+    try {
+      await writeFile(fileName, fileContent);
+    } catch {
+      console.log(errorMessage);
+    }
+  }
 };
 
-await create();
+await create(fileName, content, errorMessage);
