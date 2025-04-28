01b59fc081fca0a1d15b0693ac8a5e764ecb9cc7
Tuychiev Otabek <otabekotabek996996@gmail.com>
2025-04-28 18:01:24 +0500

    feat: add task description create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,7 @@
+// create.js - implement function that creates new file fresh.txt with 
+// content I am fresh and young inside of the files folder (if file already 
+// exists Error with message FS operation failed must be thrown)
+
 import { writeFile, access } from "fs/promises";
 import { constants } from "fs";
 
