83497c69cdcb6e09870ba4cb9118cebd8b681fc2
Tuychiev Otabek <otabekotabek996996@gmail.com>
2025-04-29 19:09:47 +0500

    feat: file system (src/fs) - delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,29 @@
-const remove = async () => {
-    // Write your code here 
+// delete.js - implement function that deletes file fileToRemove.txt
+// (if there's no file fileToRemove.txt Error with message FS
+// operation failed must be thrown)
+
+import { access, unlink } from "fs/promises";
+import { constants } from "fs";
+import { join } from "path";
+
+const sourceDir = "src/fs";
+const deleteFileName = "fileToRemove.txt";
+const errorMessage = "FS operation failed";
+
+const remove = async (src, fileName) => {
+  const filePath = join(sourceDir, deleteFileName);
+
+  try {
+    await access(filePath, constants.F_OK);
+  } catch (err) {
+    throw new Error(errorMessage);
+  }
+
+  try {
+    await unlink(filePath);
+
+    throw new Error(errorMessage);
+  } catch (err) {}
 };
 
-await remove();
+await remove(sourceDir, deleteFileName);
